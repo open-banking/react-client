cc84af764abd5fa2d75ccd0dad006b88592d8487
Steve Hu <stevehu@gmail.com>
2020-02-16 10:30:08 -0500

    fixes #4 switch to ossignin.lightapi.net and comment out token in Items

--- src/components/ResponsiveDrawer.js
+++ src/components/ResponsiveDrawer.js
@@ -71,7 +71,7 @@ class ResponsiveDrawer extends Component {
     }
 
     login() {
-        window.location = "https://signin.lightapi.net?client_id=f7d42348-c647-4efb-a52d-4c5787421e74&user_type=customer&redirect_uri=https://ob.lightapi.net/authorization&state=1222";
+        window.location = "https://obsignin.lightapi.net?client_id=f7d42348-c647-4efb-a52d-4c5787421e74&user_type=customer&redirect_uri=https://ob.lightapi.net/authorization&state=1222";
     }
 
     render() {
